1df2df28ffd7ee9bb1ceac330498694e65e9bf55
Beilong <bt132@duke.edu>
2025-02-20 23:07:09 +0800

    update

--- recipes/export_libri2mix_funcodec.sh
+++ recipes/export_libri2mix_funcodec.sh
@@ -9,10 +9,10 @@
 #SBATCH --gres=dcu:4
 
 
-# export MIOPEN_FIND_MODE=3
-# export HSA_FORCE_FINE_GRAIN_PRICE=1
-# export NCCL_IB_HCA=mlx5_0
-# export NCCL_SOCKET_IFNAME=ib0
+export MIOPEN_FIND_MODE=3
+export HSA_FORCE_FINE_GRAIN_PRICE=1
+export NCCL_IB_HCA=mlx5_0
+export NCCL_SOCKET_IFNAME=ib0
 
 # export ROCBLAS_TENSILE_LIBPATH=/public/software/compiler/rocm/dtk-23.10/lib/rocblas/library_dcu2
 source /public/software/apps/DeepLearning/whl/env.sh
