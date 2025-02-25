42e7c01997703767af0072f2bc9a31612f1cdbe6
Beilong <bt132@duke.edu>
2025-02-25 18:25:11 +0800

    update

--- src/eval/dnsmos.py
+++ src/eval/dnsmos.py
@@ -8,7 +8,7 @@ import glob
 import os
 from pathlib import Path
 import sys 
-sys.path.append(os.getcwd())
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 import librosa
 import numpy as np
