0bc9de92dc4e67a2017fa1850c36c4a74759e419
Beilong <bt132@duke.edu>
2025-03-13 20:22:13 +0800

    update

--- laura_gpt_tse_only_clean_output/src/bin/tse_inference.py
+++ laura_gpt_tse_only_clean_output/src/bin/tse_inference.py
@@ -5,12 +5,13 @@ from funcodec.torch_utils.load_pretrained_model import load_pretrained_model
 from funcodec.tasks.text2audio_generation import Text2AudioGenTask
 from funcodec.utils.misc import statistic_model_parameters
 from funcodec.bin.codec_inference import Speech2Token
+from _funcodec import build_model
 
 
 class TSExtraction:
     def __init__(self, args: Namespace, model_ckpt: str, device, only_lm, logger):
         # Load Laura GPT Model #
-        model: nn.Module = Text2AudioGenTask.build_model(args)
+        model: nn.Module = build_model(args)
         model.to(device)
         for p in args.init_param:
             load_pretrained_model(
@@ -47,25 +48,30 @@ class TSExtraction:
         # sampling and beam_size
         self.sampling = args.sampling
         self.beam_size = args.beam_size
+
+        # only lm
         self.only_lm = only_lm
 
     @torch.no_grad()
-    def __call__(self, mix_mel:torch.Tensor, ref_mel:torch.Tensor, ref_codec:torch.Tensor):
+    def __call__(self, mix_mel:torch.Tensor, ref_mel:torch.Tensor):
         """
         This function can also be used as TSE Inference.
         mix_mel the mep spec of the mixture: [1, T, D]
         ref_mel is the reference mel : [1, T, D]
-        ref_codec is the [T, N_Q], the n_q can be actually larger than the predict_nq
         """
-        text = torch.cat([ref_mel, mix_mel], dim = 1) # [1,T',D]
-        ref_codec = ref_codec[:, :self.model.predict_nq] # [T, 2]
-        ref_codec = ref_codec.tolist()
-        continual = ref_codec
-        continual_length = len(continual)
+        continual = None
+        continual_length = None
+        # text = torch.cat([ref_mel, mix_mel], dim = 1) # [1,T',D]
+        # 1. Encode mix mel and ref mel
+        mix_mel_lens = torch.tensor([mix_mel.size(1)], dtype=torch.long, device=mix_mel.device) # [1]
+        aux_mel_lens = torch.tensor([ref_mel.size(1)], dtype=torch.long, device=ref_mel.device) # [1]
 
-        # 1. Encode Text(Mel)
-        text_lens = torch.tensor([text.size(1)], dtype=torch.long, device=text.device)
-        text_outs, text_out_lens = self.model.encode(text, text_lens)
+        mix, _ = self.model.encode(mix_mel, mix_mel_lens) # [1,T,D]
+        aux, _ = self.model.encode(ref_mel, aux_mel_lens) # [1,T,D]
+        sep = self.model.lm_embedding(torch.tensor([[self.model.sep]], dtype = torch.int64, device = mix_mel.device)) # [1,1,D]
+        text_outs = torch.cat([aux, sep, mix], dim = 1) # [1, T', D]
+        text_out_lens = torch.tensor([text_outs.size(1)], dtype=torch.long, device=text_outs.device) # [1]
+
         # 2. decode first codec group
         decoded_codec = self.model.decode_codec(
             text_outs,
